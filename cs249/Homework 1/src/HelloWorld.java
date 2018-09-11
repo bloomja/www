5eed39afe7e50e2744624209c42ae332343068a8
bloomja <bloomja@sunyit.edu>
2018-09-11 16:19:09 -0400

    added homework 1

---
+++ cs249/Homework 1/src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World!");
+		System.out.println("My name is Jonathan Bloom.");
+		System.out.println("Here is 2+2: " + (2+2));
+		System.out.println("String " + "concatenation!");
+		System.out.println("Random number: " + (int)(Math.random()*100));
+		System.out.println("That is all for now, thank you!");
+		
+	}
+	
+}
